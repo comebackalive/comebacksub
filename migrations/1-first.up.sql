4ec25798429c13691423c302c835eb3fc29d3e1c
Alexander Solovyov <alexander@solovyov.net>
2022-03-03 15:59:34 +0200

    rework migrations

---
+++ migrations/1-first.up.sql
@@ -0,0 +1,43 @@
+create table if not exists users (
+        id serial primary key,
+        name text,
+        email text not null unique,
+        phone text,
+        created_at timestamp with time zone not null default now(),
+        updated_at timestamp with time zone
+);
+--;;
+
+create type transaction_type as enum ('Scheduled','Created','Settled','InProcessing','Approved','Pending', 'Expired', 'Refunded', 'Voided', 'Declined');
+--;;
+
+create table if not exists transaction_log (
+   id               serial primary key,
+   transaction      uuid  not null,
+   amount           integer not null,
+   type             transaction_type,
+   user_id          integer references users (id) not null,
+   created_at       timestamp with time zone not null default now()
+);
+--;;
+
+
+create table if not exists cards (
+   id               serial primary key,
+   user_id          integer references users (id) not null,
+   token            text not null,
+   card_pan         text,
+   card_info        jsonb,
+   is_deleted       boolean default false,
+   created_at       timestamp with time zone not null default now()
+);
+--;;
+
+create table if not exists payment_settings (
+      id               serial primary key,
+      default_card_id  integer references cards(id),
+      user_id          integer references users(id),  
+      shedule_offset   integer,
+      created_at       timestamp with time zone not null default now(),
+      updated_at       timestamp with time zone
+);
--- migrations/1-users.up.sql
+++
@@ -1,1 +0,0 @@
-alter table users add constraint users_email_key unique (email);
