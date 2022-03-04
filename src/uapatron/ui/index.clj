aafa99af28ce03879c5b53e8ae7a2969990b6673
Alexander Solovyov <alexander@solovyov.net>
2022-03-04 09:40:12 +0200

    structure handlers/templates better

--- src/uapatron/ui/index.clj
+++ src/uapatron/ui/index.clj
@@ -5,35 +5,49 @@
             [uapatron.db :as db]))
 
 
+(defn login-sent-t [{:keys [email]}]
+  (base/wrap
+    [:p
+     "Authentication link has been sent to "
+     email
+     ". Please open the link to log in - it's going to be valid for 5 minutes."]))
+
+
+(defn dash-t []
+  (base/wrap
+    [:h1 "Hello, " (:email (auth/user))]))
+
+
+(defn anon-t []
+  (base/wrap
+    [:form {:method "post" :action "/login"}
+     [:label "Email"
+      [:input {:type "email" :name "email" :required true}]]
+     [:button {:name "login"} "Login"]]))
+
+
+;;; HTTP views
+
+
 (defn page [_req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body
-   (if (auth/uid)
-     (base/wrap
-       [:h1 "Hello, " (:email (auth/user))])
-     (base/wrap
-       [:form {:method "post" :action "/login"}
-        [:label "Email"
-         [:input {:type "email" :name "email" :required true}]]
-        [:button {:name "login"} "Login"]]))})
+   :body    (if (auth/uid)
+              (dash-t)
+              (anon-t))})
 
 
 (defn start-login [{:keys [request-method form-params] :as req}]
   (if (not= request-method :post)
     {:status 405
-     :body "Method Not Allowed"}
+     :body   "Method Not Allowed"}
     (if-let [email (get form-params "email")]
       (do
         (email/email-auth! email)
-        {:status 200
+        {:status  200
          :headers {"Content-Type" "text/html"}
-         :body (base/wrap
-                 [:p
-                  "Authentication link has been sent to "
-                  email
-                  ". Please open the link to log in - it's going to be valid for 5 minutes."])})
-      {:status 302
+         :body    (login-sent-t {:email email})})
+      {:status  302
        :headers {"Location" "/?error=email-empty"}})))
 
 
