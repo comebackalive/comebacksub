623b11592d2d882a75992da754e0f267731e5f8d
Alexander Solovyov <alexander@solovyov.net>
2022-03-05 15:34:25 +0200

    scheduler

--- src/uapatron/bl/schedule.clj
+++ src/uapatron/bl/schedule.clj
@@ -1,27 +1,62 @@
 (ns uapatron.bl.schedule
-  (:require [uapatron.db :as db]
+  (:import [java.time LocalTime])
+  (:require [clojure.tools.logging :as log]
+
+            [uapatron.db :as db]
             [uapatron.bl.fondy :as fondy]))
 
 
 (def BATCH 100)
+(def NEXT-DAY-CHARGE
+  "Time when we start charging for the next day"
+  (LocalTime/of 12 0))
 
 
-(defn uids-to-charge-q
-  []
+(defn uids-to-charge-q [which-day]
   {:from     [[:payment_settings :ps]]
    :select   [:ps.id]
    :where    [:and [:< :next_payment_at
-                    [:raw "current_date + 1"]]
+                    (case which-day
+                      :yesterday [:raw "current_date"]
+                      :today     [:raw "current_date + 1"]
+                      :tomorrow  [:raw "current_date + 2"])]
               [:not= :ps.amount nil]
               [:not= :ps.card_id nil]]
    :order-by [[:ps.id :desc]]
-   :limit    100})
+   :limit    1000})
 
 
 (defn process-scheduled!
-  []
-  (let [ids (db/q (uids-to-charge-q))]
-    (for [item ids]
-      (fondy/process-recurrent-payment! (:id item)))
-    (when (= (count ids) 100)
-      (recur))))
+  ([]
+   (process-scheduled!
+     (if (.isBefore (LocalTime/now) NEXT-DAY-CHARGE)
+       :yesterday
+       :today)))
+  ([which-day]
+   (let [ids (db/q (uids-to-charge-q which-day))]
+     (for [item ids]
+       (fondy/process-recurrent-payment! (:id item))))))
+
+
+(defn run-schedule []
+  (let [stop (atom false)
+        t (Thread.
+            (fn []
+              (log/debug "schedule")
+              (if @stop
+                (log/info "stop")
+
+                (do
+                  (try
+                    (process-scheduled!)
+                    (catch Exception e
+                      (log/error e "cron error")))
+                  (try
+                    (Thread/sleep 300000)
+                    (catch InterruptedException _
+                      (log/info "stop")))
+                  (recur)))))]
+    (.start t)
+    (fn []
+      (reset! stop true)
+      (.interrupt t))))
--- src/uapatron/main.clj
+++ src/uapatron/main.clj
@@ -5,11 +5,17 @@
             [org.httpkit.sni-client :as sni-client]
 
             [uapatron.httpd]
+            [uapatron.bl.schedule]
             [uapatron.db]))
 
 
 (set! *warn-on-reflection* true)
 (alter-var-root #'http/*default-client* (fn [_] sni-client/default-client))
+
+
+(mount/defstate schedule
+  :start (uapatron.bl.schedule/run-schedule)
+  :stop  (schedule))
 
 
 (defn -main [& args]
